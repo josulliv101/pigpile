477c71ec7d3b75ed1935408a27193524f51c86d8
Joe Sullivan <josulliv101@gmail.com>
2022-07-05 15:18:03 -0400

    prettier updates

--- packages/composites/src/forms/LoginForm/LoginForm.stories.tsx
+++ packages/composites/src/forms/LoginForm/LoginForm.stories.tsx
@@ -1,11 +1,15 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { screen, userEvent } from "@storybook/testing-library";
 import { LoginForm } from "./LoginForm";
 
 export default {
   title: "Forms/ Login Form",
   component: LoginForm,
   argTypes: { onSignInWithProvider: { action: "provider btn clicked" } },
-};
+} as ComponentMeta<typeof LoginForm>;
 
-export const Basic = (args) => {
-  return <LoginForm {...args} />;
-};
+const Template: ComponentStory<typeof LoginForm> = (args) => (
+  <LoginForm {...args} />
+);
+
+export const Basic = Template.bind({});
