b2fe48e81d762f22c7a108e9195f0e9b6c05f503
Joe Sullivan <josulliv101@gmail.com>
2022-08-01 16:03:15 -0400

    rename variable

--- apps/nextjs/store/middlewareAugmentActionPayloadWithId.ts
+++ apps/nextjs/store/middlewareAugmentActionPayloadWithId.ts
@@ -1,16 +1,15 @@
-const effectedActions = ["status/setStatus", "donation/add/rejected"];
+const actions = ["status/setStatus", "donation/add/rejected"];
 
-// TODO update id generator if needed
+// TODO update id generator
 const getId = () => new Date().getTime().toString(36);
 
-export const middlewareAugmentActionPayloadWithId =
-  () => (next) => (action) => {
-    if (!effectedActions.includes(action.type)) {
-      return next(action);
-    }
-    let result = next({
-      ...action,
-      payload: { ...action.payload, id: getId() },
-    });
-    return result;
-  };
+export const middlewareAugmentActionPayloadWithId = () => (next) => (action) => {
+  if (!actions.includes(action.type)) {
+    return next(action);
+  }
+  let result = next({
+    ...action,
+    payload: { ...action.payload, id: getId() },
+  });
+  return result;
+};
