a2282e6b557680b1bc7d93fb47d5cc1570cd0073
Joe Sullivan <josulliv101@gmail.com>
2022-08-02 13:25:59 -0400

    update mobile footer

--- apps/nextjs/components/landmarks/Footer/FooterSmall.tsx
+++ apps/nextjs/components/landmarks/Footer/FooterSmall.tsx
@@ -1,6 +1,7 @@
 import NextLink from "next/link";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 import {
+  Box,
   ButtonGroup,
   Button,
   Container,
@@ -22,39 +23,38 @@ const NavLink = ({ href, children }) => (
 
 export const FooterSmall: React.FC<HTMLChakraProps<"footer">> = (props) => {
   return (
-    <Container
-      as="footer"
-      role="contentinfo"
-      py={{ base: "2", md: "4" }}
-      {...props}
-    >
-      <Stack spacing={{ base: "4", md: "5" }} opacity=".9">
-        <Stack justify="space-between" direction="row" align="center">
-          <HStack>
-            <Text fontSize="sm" color="gray.300">
-              &copy; {new Date().getFullYear()} Pigpile Corporation. All rights
-              reserved.
-            </Text>
-            {/*<Logo boxSize={5} bgColor="gray.600" fill="gray.500" shadow="none" borderWidth="0" opacity=".4" />*/}
-          </HStack>
-          <Show above="sm">
-            <ButtonGroup variant="ghost" spacing="0" colorScheme="blackAlpha">
-              <NavLink href="/about">About</NavLink>
-              <NavLink href="/about">Privacy</NavLink>
-              <NavLink href="/about">Terms of Use</NavLink>
-              <IconButton
-                as="a"
-                href="https://github.com/josulliv101"
-                target="_blank"
-                aria-label="GitHub"
-                color="gray.200"
-                size="sm"
-                icon={<FaGithub fontSize="1.0rem" />}
-              />
-            </ButtonGroup>
-          </Show>
+    <Box pt="8" bg="linear-gradient(rgba(0,0,0,0), rgba(28,41,29,.6))" {...props}>
+      <Container as="footer" role="contentinfo" py="2">
+        <Stack spacing={{ base: "4", md: "5" }} opacity=".9">
+          <Stack justify="space-between" direction="row" align="center">
+            <HStack>
+              <Text fontSize="sm" color="gray.300">
+                &copy; {new Date().getFullYear()} Pigpile Corporation.{" "}
+                <Box as="span" whiteSpace="nowrap">
+                  All rights reserved.
+                </Box>
+              </Text>
+              {/*<Logo boxSize={5} bgColor="gray.600" fill="gray.500" shadow="none" borderWidth="0" opacity=".4" />*/}
+            </HStack>
+            <Show above="sm">
+              <ButtonGroup variant="ghost" spacing="0" colorScheme="blackAlpha">
+                <NavLink href="/about">About</NavLink>
+                <NavLink href="/about">Privacy</NavLink>
+                <NavLink href="/about">Terms of Use</NavLink>
+                <IconButton
+                  as="a"
+                  href="https://github.com/josulliv101"
+                  target="_blank"
+                  aria-label="GitHub"
+                  color="gray.200"
+                  size="sm"
+                  icon={<FaGithub fontSize="1.0rem" />}
+                />
+              </ButtonGroup>
+            </Show>
+          </Stack>
         </Stack>
-      </Stack>
-    </Container>
+      </Container>
+    </Box>
   );
 };
