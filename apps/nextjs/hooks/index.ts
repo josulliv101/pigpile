085af94cdd3f723549d6adc7299afa2e7a2e577e
Joe Sullivan <josulliv101@gmail.com>
2022-08-03 09:15:50 -0400

    type tweaks

--- apps/nextjs/hooks/index.ts
+++ apps/nextjs/hooks/index.ts
@@ -1,6 +1,13 @@
+import { useDispatch, useSelector } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
+import type { AppState, AppDispatch } from "store";
+
 export * from "./useLabelBundle";
 export * from "./useConnectClient";
 export * from "./useNavProps";
 export * from "./useDonationsSubscription";
 export * from "./useStatusManager";
 export * from "./useTheme";
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
--- apps/nextjs/store/store.ts
+++ apps/nextjs/store/store.ts
@@ -36,6 +36,6 @@ export const makeStore = () =>
 
 export type AppStore = ReturnType<typeof makeStore>;
 export type AppState = ReturnType<AppStore["getState"]>;
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unknown, Action>;
+export type AppDispatch = AppStore["dispatch"];
 
 export const wrapper = createWrapper<AppStore>(makeStore);
