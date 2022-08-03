6e3ad56e774a2b290373edaa70effcfce30206a3
Joe Sullivan <josulliv101@gmail.com>
2022-08-03 10:06:45 -0400

    add useRouteChangeListeners hook

--- apps/nextjs/hooks/useRouteChangeListeners.ts
+++ apps/nextjs/hooks/useRouteChangeListeners.ts
@@ -10,9 +10,6 @@ export function useRouteChangeListeners() {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    const handleRouteChangeStart = (url: sting) => {
-      console.log(`Loading: ${url}`);
-    };
     const handleRouteChangStop = (url: string) => {
       if (isMobileNavOpen) {
         setTimeout(() => dispatch(appSlice.actions.closeMobileNav()), 0);
@@ -20,12 +17,10 @@ export function useRouteChangeListeners() {
       ga.pageview(url);
     };
 
-    router.events.on("routeChangeStart", handleRouteChangeStart);
     router.events.on("routeChangeComplete", handleRouteChangStop);
     router.events.on("routeChangeError", handleRouteChangStop);
 
     return () => {
-      router.events.off("routeChangeStart", handleRouteChangeStart);
       router.events.off("routeChangeComplete", handleRouteChangStop);
       router.events.off("routeChangeError", handleRouteChangStop);
     };
