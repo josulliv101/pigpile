eee5561cc55f28c32ffb33d1e75ef299296b1ba7
Joe Sullivan <josulliv101@gmail.com>
2022-08-08 09:25:36 -0400

    fix typing

--- apps/nextjs/analytics.ts
+++ apps/nextjs/analytics.ts
@@ -1,12 +1,17 @@
 export const pageview = (url: string) => {
   if (typeof window !== "undefined" && window.gtag) {
-    window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+    window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS as string, {
       page_path: url,
     });
   }
 };
 
-export const event = ({ action, params }) => {
+export type GTagEvent = {
+  action: string;
+  params: Gtag.CustomParams
+};
+
+export const event = ({ action, params }: GTagEvent) => {
   if (typeof window !== "undefined" && window.gtag) {
     window.gtag("event", action, params);
   }
